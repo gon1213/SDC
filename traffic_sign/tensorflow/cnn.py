92104527c0fa3226e4019404870dd0fc2c2bf8fc
gon1213 <gon901213@gmail.com>
2017-01-23 02:38:23 -0800

    update: finishing project 2, start Keras

---
+++ traffic_sign/tensorflow/cnn.py
@@ -0,0 +1,144 @@
+###
+out_height = ceil(float(in_height - filter_height + 1) / float(strides[1]))
+out_width  = ceil(float(in_width - filter_width + 1) / float(strides[2]))
+
+
+
+#Dimensionality
+#From what we've learned so far, how can we calculate the number of neurons of each layer in our CNN?
+#
+#Given our input layer has a volume of W, our filter has a volume (height * width * depth) of F, 
+#we have a stride of S, and a padding of P, the following formula gives us the volume of the next layer: (W−F+2P)/S+1.
+
+new_height = (input_height - filter_height + 2 * P)/S + 1
+new_width = (input_width - filter_width + 2 * P)/S + 1
+
+#convolution output 
+
+input = tf.placeholder(tf.float32, (None, 32, 32, 3))
+filter_weights = tf.Variable(tf.truncated_normal((8, 8, 3, 20))) # (height, width, input_depth, output_depth)
+filter_bias = tf.Variable(tf.zeros(20))
+strides = [1, 2, 2, 1] # (batch, height, width, depth)
+padding = 'VALID'
+conv = tf.nn.conv2d(input, filter_weights, strides, padding) + filter_bias
+#conv [1,13,13,20]
+#
+#if padding = "SAME"
+#conv [1,16,16,20]
+
+###### tensorflow convolution layer
+
+# Output depth
+k_output = 64
+
+# Image Properties
+image_width = 10
+image_height = 10
+color_channels = 3
+
+# Convolution filter
+filter_size_width = 5
+filter_size_height = 5
+
+# Input/Image
+input = tf.placeholder(
+    tf.float32,
+    shape=[None, image_width, image_height, color_channels])
+
+# Weight and bias
+weight = tf.Variable(tf.truncated_normal(
+    [filter_size_width, filter_size_height, color_channels, k_output]))
+bias = tf.Variable(tf.zeros(k_output))
+
+# Apply Convolution
+conv_layer = tf.nn.conv2d(input, weight, strides=[1, 2, 2, 1], padding='SAME')
+# Add bias
+conv_layer = tf.nn.bias_add(conv_layer, bias)
+# Apply activation function
+conv_layer = tf.nn.relu(conv_layer)
+
+###### cnn in tensorflow
+
+# tf Graph input
+x = tf.placeholder(tf.float32, [None, 28, 28, 1])
+y = tf.placeholder(tf.float32, [None, n_classes])
+keep_prob = tf.placeholder(tf.float32)
+
+# Model
+logits = conv_net(x, weights, biases, keep_prob)
+
+# Define loss and optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, y))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)\
+    .minimize(cost)
+
+# Accuracy
+correct_pred = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+# Initializing the variables
+init = tf. global_variables_initializer()
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(epochs):
+        for batch in range(mnist.train.num_examples//batch_size):
+            batch_x, batch_y = mnist.train.next_batch(batch_size)
+            sess.run(optimizer, feed_dict={
+                x: batch_x,
+                y: batch_y,
+                keep_prob: dropout})
+
+            # Calculate batch loss and accuracy
+            loss = sess.run(cost, feed_dict={
+                x: batch_x,
+                y: batch_y,
+                keep_prob: 1.})
+            valid_acc = sess.run(accuracy, feed_dict={
+                x: mnist.validation.images[:test_valid_size],
+                y: mnist.validation.labels[:test_valid_size],
+                keep_prob: 1.})
+
+            print('Epoch {:>2}, Batch {:>3} -'
+                  'Loss: {:>10.4f} Validation Accuracy: {:.6f}'.format(
+                epoch + 1,
+                batch + 1,
+                loss,
+                valid_acc))
+
+    # Calculate Test Accuracy
+    test_acc = sess.run(accuracy, feed_dict={
+        x: mnist.test.images[:test_valid_size],
+        y: mnist.test.labels[:test_valid_size],
+        keep_prob: 1.})
+    print('Testing Accuracy: {}'.format(test_acc))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- traffic_sign/tensorflow/deep_network.py
+++ traffic_sign/tensorflow/deep_network.py
@@ -7,4 +7,235 @@ hidden_layer = tf.nn.dropout(hidden_layer, keep_prob)
 logits = tf.add(tf.matmul(hidden_layer, weights[1]), biases[1])
 
 #########
+init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+    print(sess.run(logits, feed_dict={keep_prob: 0.5}))
+########
+###tensirflow minist
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets(".", one_hot=True, reshape=False)
+
+###learning parameter
+
+# Parameters
+learning_rate = 0.001
+training_epochs = 20
+batch_size = 128  # Decrease batch size if you don't have enough memory
+display_step = 1
+
+n_input = 784  # MNIST data input (img shape: 28*28)
+n_classes = 10  # MNIST total classes (0-9 digits)
+
+###hidden layer parameters
+n_hidden_layer = 256 # layer number of features
+
+###weight and biases
+
+# Store layers weight & bias
+weights = {
+    'hidden_layer': tf.Variable(tf.random_normal([n_input, n_hidden_layer])),
+    'out': tf.Variable(tf.random_normal([n_hidden_layer, n_classes]))
+}
+biases = {
+    'hidden_layer': tf.Variable(tf.random_normal([n_hidden_layer])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+
+###input
+# tf Graph input
+x = tf.placeholder("float", [None, 28, 28, 1])
+y = tf.placeholder("float", [None, n_classes])
+
+x_flat = tf.reshape(x, [-1, n_input])
+
+###multilater perceptron
+# Hidden layer with RELU activation
+layer_1 = tf.add(tf.matmul(x_flat, weights['hidden_layer']), biases['hidden_layer'])
+layer_1 = tf.nn.relu(layer_1)
+# Output layer with linear activation
+logits = tf.add(tf.matmul(layer_1, weights['out']), biases['out'])
+
+###optimizer
+# Define loss and optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, y))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+
+###session
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+    # Training cycle
+    for epoch in range(training_epochs):
+        total_batch = int(mnist.train.num_examples/batch_size)
+        # Loop over all batches
+        for i in range(total_batch):
+            batch_x, batch_y = mnist.train.next_batch(batch_size)
+            # Run optimization op (backprop) and cost op (to get loss value)
+            sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
+
+
+###save tensorflow
+
+import tensorflow as tf
+
+# The file path to save the data
+save_file = 'model.ckpt'
+
+# Two Tensor Variables: weights and bias
+weights = tf.Variable(tf.truncated_normal([2, 3]))
+bias = tf.Variable(tf.truncated_normal([3]))
+
+# Class used to save and/or restore Tensor Variables
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    # Initialize all the Variables
+    sess.run(tf.global_variables_initializer())
+
+    # Show the values of weights and bias
+    print('Weights:')
+    print(sess.run(weights))
+    print('Bias:')
+    print(sess.run(bias))
+
+    # Save the model
+    saver.save(sess, save_file)
+
+### restore tensorflow
+
+# Remove the previous weights and bias
+tf.reset_default_graph()
+
+# Two Variables: weights and bias
+weights = tf.Variable(tf.truncated_normal([2, 3]))
+bias = tf.Variable(tf.truncated_normal([3]))
+
+# Class used to save and/or restore Tensor Variables
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    # Load the weights and bias
+    saver.restore(sess, save_file)
+
+    # Show the values of weights and bias
+    print('Weight:')
+    print(sess.run(weights))
+    print('Bias:')
+    print(sess.run(bias))
+
+### avoid name error
+
+import tensorflow as tf
+
+tf.reset_default_graph()
+
+save_file = 'model.ckpt'
+
+# Two Tensor Variables: weights and bias
+weights = tf.Variable(tf.truncated_normal([2, 3]), name='weights_0')
+bias = tf.Variable(tf.truncated_normal([3]), name='bias_0')
+
+saver = tf.train.Saver()
+
+# Print the name of Weights and Bias
+print('Save Weights: {}'.format(weights.name))
+print('Save Bias: {}'.format(bias.name))
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    saver.save(sess, save_file)
+
+# Remove the previous weights and bias
+tf.reset_default_graph()
+
+# Two Variables: weights and bias
+bias = tf.Variable(tf.truncated_normal([3]), name='bias_0')
+weights = tf.Variable(tf.truncated_normal([2, 3]) ,name='weights_0')
+
+saver = tf.train.Saver()
+
+# Print the name of Weights and Bias
+print('Load Weights: {}'.format(weights.name))
+print('Load Bias: {}'.format(bias.name))
+
+with tf.Session() as sess:
+    # Load the weights and bias - No Error
+    saver.restore(sess, save_file)
+
+print('Loaded Weights and Bias successfully.')
+
+#######regularization
+###dropout 
+
+keep_prob = tf.placeholder(tf.float32) # probability to keep units
+
+hidden_layer = tf.add(tf.matmul(features, weights[0]), biases[0])
+hidden_layer = tf.nn.relu(hidden_layer)
+hidden_layer = tf.nn.dropout(hidden_layer, keep_prob)
+
+logits = tf.add(tf.matmul(hidden_layer, weights[1]), biases[1])
+
+#### apply ReLU and dropout
+
+import tensorflow as tf
+
+hidden_layer_weights = [
+    [0.1, 0.2, 0.4],
+    [0.4, 0.6, 0.6],
+    [0.5, 0.9, 0.1],
+    [0.8, 0.2, 0.8]]
+out_weights = [
+    [0.1, 0.6],
+    [0.2, 0.1],
+    [0.7, 0.9]]
+
+# Weights and biases
+weights = [
+    tf.Variable(hidden_layer_weights),
+    tf.Variable(out_weights)]
+biases = [
+    tf.Variable(tf.zeros(3)),
+    tf.Variable(tf.zeros(2))]
+
+# Input
+features = tf.Variable([[0.0, 2.0, 3.0, 4.0], [0.1, 0.2, 0.3, 0.4], [11.0, 12.0, 13.0, 14.0]])
+
+# TODO: Create Model with Dropout
+keep_prob = tf.placeholder(tf.float32)
+hidden_layer = tf.add(tf.matmul(features, weights[0]), biases[0])
+hidden_layer = tf.nn.relu(hidden_layer)
+hidden_layer = tf.nn.dropout(hidden_layer, keep_prob)
+
+logits = tf.add(tf.matmul(hidden_layer, weights[1]), biases[1])
+
+# TODO: Print logits from a session
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    print(sess.run(logits, feed_dict={keep_prob: 0.5}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
